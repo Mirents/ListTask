0d2a6784bcf48b8f0458692c624d623af140c524
VadMih <mirent@mail.ru>
2019-12-03 23:00:04 +0300

    Read file MyTask.java

--- src/listtask/MyTask.java
+++ src/listtask/MyTask.java
@@ -1,5 +1,21 @@
 package listtask;
 
 public class MyTask {
+	private String description; // Описание задачи
+	private int priority;       // Приоритет: от 1 до 4
+	private int influence;      // Влияние: от 1 до 3
+	
+	// Конструктор класса с параметрами
+	MyTask(String description, int priority, int influence) {
+		this.description = description;
+		this.priority = priority;
+		this.influence = influence;
+	}
 
+	// Конструктор класса без параметров
+	MyTask() {
+		this.description = "";
+		this.priority = 0;
+		this.influence = 0;
+	}
 }
