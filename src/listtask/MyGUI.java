e2d7f549a0e70e94124a01f0dd314b404d200174
VadMih <mirent@mail.ru>
2019-12-05 00:21:12 +0300

    Now read file, work version

--- src/listtask/MyGUI.java
+++ src/listtask/MyGUI.java
@@ -13,32 +13,37 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextPane;
+import javax.swing.DropMode;
+import javax.swing.JList;
 
 public class MyGUI extends JFrame {
-	MyListTask listTask;
+	private MyListTask listTask;
 	private JTextPane textPane;
-	JButton myButton;
+	private JButton myButton;
 	private JPanel contentPane;
-	ButtonEventListener buttonEventListener = new ButtonEventListener();
+	private ButtonEventListener buttonEventListener;
 	
 	public MyGUI() {		
 		super("Test");
-		setResizable(false);
+		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(100, 100, 390, 453);
+		//this.setBounds(100, 100, 390, 453);
+		this.setSize(461, 453);
+		this.setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		this.setContentPane(contentPane);
 		
-		myButton = new JButton("Прочитать список задач из файла");
-		myButton.setBounds(12, 12, 366, 25);
+		myButton = new JButton("Open List Task");
+		myButton.setBounds(12, 12, 134, 25);
 		contentPane.add(myButton);
 		
 		textPane = new JTextPane();
-		textPane.setBounds(12, 61, 366, 350);
+		textPane.setBounds(12, 43, 218, 368);
 		contentPane.add(textPane);
 		
+		buttonEventListener = new ButtonEventListener();
 		myButton.addActionListener(buttonEventListener);
 		
 		// Создание списка задач
@@ -50,6 +55,7 @@ public class MyGUI extends JFrame {
 	class ButtonEventListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			try {
 			if (e.getSource() == myButton) {
 				String str = "";
 				
@@ -74,7 +80,10 @@ public class MyGUI extends JFrame {
 				str += ("\nTask before sort - METHOD_INFLUENCE\n" + listTask.getAllText());
 				textPane.setText(str);
 			}
-			
+			// else JOptionPane.showMessageDialog(null, "Add Done", "Output", JOptionPane.PLAIN_MESSAGE);
+			} catch(Exception eX) {
+				JOptionPane.showMessageDialog(null, "Error!");				
+			}
 		}
 	}
 }
